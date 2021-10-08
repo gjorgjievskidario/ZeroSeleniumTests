235b5e7f00fd45282eb4aea91160bc6650b570d3
gjorgjievski <gjorgjievskidario@yahoo.com>
2021-10-08 14:05:03 +0200

    SeleniumTest second

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -1,10 +1,13 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+public class HomePage extends BasePage {
 
-public class HomePage {
-    public HomePage(){}
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
 
     @FindBy(id="signin_button")
     private WebElement loginButton;
--- src/test/java/LoginFeatureTest.java
+++ src/test/java/LoginFeatureTest.java
@@ -1,24 +1,67 @@
+import Pages.AccountSummaryPage;
 import Pages.HomePage;
+import Pages.LoginPage;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
 
-public class LoginFeatureTest {
+class LoginFeatureTests {
     private static final WebDriver driver = new ChromeDriver();
-   static  HomePage homePage = new HomePage();
+    static ChromeOptions chromeOptions = new ChromeOptions();
 
+    static HomePage homePage = new HomePage(driver);
+    static LoginPage loginPage = new LoginPage(driver);
+    static AccountSummaryPage accountSummaryPage = new AccountSummaryPage(driver);
+    public static String expectedHeaderString = "Log in to ZeroBank";
 
-    @Test
-    public void successfulLoginTest(){
-        driver.get("http://zero.webappsecurity.com/index.html");
-        homePage.clickElement();
-
-
-
-
-
-
-        driver.quit();
+    @BeforeClass
+    public void beforeClass() {
+        driver.get("http://zero.webappsecurity.com/login.html");
+        chromeOptions.addArguments("--allow-insecure-localhost");
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--ignore-certificate-errors");
     }
 
+    @Test
+    public void invalidLoginTest() {
+        loginPage.clickLoginButton();
+        String expectedMessage = "Login and/or password are wrong.";
+
+        Assert.assertEquals(loginPage.getInvalidCredentialsText(), expectedMessage);
+
+    }
+
+    @Test
+    public static void successfulLoginTest() {
+        boolean isHeaderDisplayed = loginPage.isHeaderElementDisplayed();
+        String actualHeaderText = loginPage.getHeaderText();
+
+        Assert.assertTrue(isHeaderDisplayed);
+        Assert.assertEquals(actualHeaderText, expectedHeaderString);
+
+        loginPage.enterUsername();
+        loginPage.enterPassword();
+        loginPage.clickLoginButton();
+
+        String currentUrl = driver.getCurrentUrl();
+        accountSummaryPage.getText();
+
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+        driver.quit();
+    }
 }
+
+
+
+
+
+
